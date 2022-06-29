fe50c78aca59c0d1be9631991b2a4ae349be123d
USER <twodf78@naver.com>
2022-06-29 22:41:29 +0900

    tab2

--- app/src/main/java/com/example/project1/gallery.java
+++ app/src/main/java/com/example/project1/gallery.java
@@ -4,20 +4,35 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.ImageView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
 
+import java.net.DatagramPacket;
+
 public class gallery extends AppCompatActivity {
+    private final int GET_GALLERY_IMAGE = 200;
+    private ImageView imageview;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
+        imageview = (ImageView) findViewById(R.id.imageView);
+        imageview.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
 
+                Intent intent = new Intent(Intent.ACTION_PICK);
+                intent.setDataAndType(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
+                startActivityForResult(intent, GET_GALLERY_IMAGE);
+            }
+        });
         // Initialize and assign variable
-        BottomNavigationView bottomNavigationView=findViewById(R.id.bottom_navigation);
+        BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 //
 //        // Set Home selected
 //        bottomNavigationView.setSelectedItemId(R.id.home);
@@ -27,22 +42,33 @@ public class gallery extends AppCompatActivity {
             @Override
             public boolean onNavigationItemSelected(@NonNull MenuItem item) {
 
-                switch(item.getItemId())
-                {
+                switch (item.getItemId()) {
                     case R.id.phonebook:
                         startActivity(new Intent(getApplicationContext(), phonebook.class));
-                        overridePendingTransition(0,0);
+                        overridePendingTransition(0, 0);
                         return true;
                     case R.id.gallary:
                         return true;
                     case R.id.about:
                         startActivity(new Intent(getApplicationContext(), about.class));
-                        overridePendingTransition(0,0);
+                        overridePendingTransition(0, 0);
                         return true;
                 }
                 return false;
             }
         });
+    }
+    @Override
+    protected void onActivityResult(int requestCode,int resultCode, Intent data){
+
+        if (requestCode == GET_GALLERY_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
+
+            Uri selectedImageUri = data.getData();
+            imageview.setImageURI(selectedImageUri);
+
+        }
 
     }
 }
+
+
