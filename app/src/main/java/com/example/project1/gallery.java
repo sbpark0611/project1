add496087fc350a871a1464caa38b14f3cc64cdb
USER <twodf78@naver.com>
2022-06-30 15:46:08 +0900

    gallery

--- app/src/main/java/com/example/project1/gallery.java
+++ app/src/main/java/com/example/project1/gallery.java
@@ -8,6 +8,7 @@ import android.net.Uri;
 import android.os.Bundle;
 import android.view.MenuItem;
 import android.view.View;
+import android.widget.GridView;
 import android.widget.ImageView;
 
 import com.google.android.material.bottomnavigation.BottomNavigationView;
@@ -15,22 +16,19 @@ import com.google.android.material.bottomnavigation.BottomNavigationView;
 import java.net.DatagramPacket;
 
 public class gallery extends AppCompatActivity {
-    private final int GET_GALLERY_IMAGE = 200;
-    private ImageView imageview;
+
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_gallery);
-        imageview = (ImageView) findViewById(R.id.imageView);
-        imageview.setOnClickListener(new View.OnClickListener() {
-            public void onClick(View v) {
 
-                Intent intent = new Intent(Intent.ACTION_PICK);
-                intent.setDataAndType(android.provider.MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
-                startActivityForResult(intent, GET_GALLERY_IMAGE);
-            }
-        });
+        GridView gridView = (GridView) findViewById(R.id.grid_view);
+
+        // Instance of ImageAdapter Class
+        gridView.setAdapter(new ImageAdapter(this));
+
+
         // Initialize and assign variable
         BottomNavigationView bottomNavigationView = findViewById(R.id.bottom_navigation);
 //
@@ -58,17 +56,7 @@ public class gallery extends AppCompatActivity {
             }
         });
     }
-    @Override
-    protected void onActivityResult(int requestCode,int resultCode, Intent data){
 
-        if (requestCode == GET_GALLERY_IMAGE && resultCode == RESULT_OK && data != null && data.getData() != null) {
-
-            Uri selectedImageUri = data.getData();
-            imageview.setImageURI(selectedImageUri);
-
-        }
-
-    }
 }
 
 
