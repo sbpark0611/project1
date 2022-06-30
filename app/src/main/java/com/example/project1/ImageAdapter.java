c2fddb34978d54f4cb7754de711d076f791fdf63
USER <twodf78@naver.com>
2022-06-30 16:17:50 +0900

    gallery

--- app/src/main/java/com/example/project1/ImageAdapter.java
+++ app/src/main/java/com/example/project1/ImageAdapter.java
@@ -18,11 +18,12 @@ public class ImageAdapter extends BaseAdapter {
             R.drawable.pic_3, R.drawable.pic_4,
             R.drawable.pic_5, R.drawable.pic_6,
             R.drawable.pic_7, R.drawable.pic_8,
-            R.drawable.pic_9
-//                R.drawable.pic_10,
-//                R.drawable.pic_11, R.drawable.pic_12,
-//                R.drawable.pic_13, R.drawable.pic_14,
-//                R.drawable.pic_15
+            R.drawable.pic_9, R.drawable.pic_10,
+            R.drawable.pic_11, R.drawable.pic_12,
+            R.drawable.pic_13, R.drawable.pic_14,
+            R.drawable.pic_15, R.drawable.pic_16,
+            R.drawable.pic_17, R.drawable.pic_18,
+            R.drawable.pic_19, R.drawable.pic_20
     };
 
     // Constructor
